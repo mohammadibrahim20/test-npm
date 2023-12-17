2b923255e117a481676194fed929b1a73cd5d7f4
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-12-17 23:26:28 +0600

    first commit

---
+++ src/hooks/useCountry.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchCountryInfo = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`https://restcountries.com/v3.1/name`);
+        const data = await response.json();
+        setCountry(data);
+      } catch (error) {
+        console.error(error);
+        setError(`This country is not available!`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountryInfo();
+  }, [name]);
+  return {
+    loading,
+    error,
+    country,
+  };
+};
